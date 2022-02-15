749fbb561b8b5e9faf6d120e588085255d9e4f40
decagon <godsonjonathan39@gmail.com>
2022-02-15 17:59:30 +0100

    made two form group element to be on a row

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
-import {Button, Card, Col, Form} from "react-bootstrap";
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
 
 export default class Book extends Component{
     render(){
         return(
             <Card className={"border border-dark bg-white text-white"}>
-                <Card.Header>Add Book</Card.Header>
+                <Card.Header className={"text-dark"}>Add Book</Card.Header>
                 <Form>
-                <Card.Body>
-                        <Form.Row>
-                            <Form.Group as={Col}>
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control type="test" className={"bg-white text-dark"} placeholder="Enter Book Title" />
-                            </Form.Group>
-                            <Form.Group as={Col}>
-                                <Form.Label>Author</Form.Label>
-                                <Form.Control type="test" className={"bg-white text-dark"} placeholder="Enter Book Title" />
-                            </Form.Group>
-                        </Form.Row>
-                </Card.Body>
-                <Card.Footer style={{"textAlign":"right"}}>
-                    <Button size="sm" variant="success" type="submit">
-                        Submit
-                    </Button>
-                </Card.Footer>
-            </Form>
+                    <Card.Body>
+                       <Row>
+                           <Form.Group as={Col}>
+                               <Form.Label>Title</Form.Label>
+                               <Form.Control type="text" placeholder="Enter Title" />
+                           </Form.Group>
+                           <Form.Group as={Col}>
+                               <Form.Label>Author</Form.Label>
+                               <Form.Control type="text" placeholder="Enter Author" />
+                           </Form.Group>
+                       </Row>
+                    </Card.Body>
+                    <Card.Footer style={{"textAlign":"right"}}>>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Card.Footer>
+                </Form>
             </Card>
         );
     }
