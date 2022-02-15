cfc9cc5d7e2b9a1b84544e6d5370fbdac4a1bb1b
decagon <godsonjonathan39@gmail.com>
2022-02-15 17:37:21 +0100

    Added card to Book and Book List

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import {Button, Card, Col, Form} from "react-bootstrap";
 
 export default class Book extends Component{
     render(){
         return(
-            <div className="text-white-50">Add Book</div>
-        )
+            <Card className={"border border-dark bg-white text-white"}>
+                <Card.Header>Add Book</Card.Header>
+                <Form>
+                <Card.Body>
+                        <Form.Row>
+                            <Form.Group as={Col}>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="test" className={"bg-white text-dark"} placeholder="Enter Book Title" />
+                            </Form.Group>
+                            <Form.Group as={Col}>
+                                <Form.Label>Author</Form.Label>
+                                <Form.Control type="test" className={"bg-white text-dark"} placeholder="Enter Book Title" />
+                            </Form.Group>
+                        </Form.Row>
+                </Card.Body>
+                <Card.Footer style={{"textAlign":"right"}}>
+                    <Button size="sm" variant="success" type="submit">
+                        Submit
+                    </Button>
+                </Card.Footer>
+            </Form>
+            </Card>
+        );
     }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import {Card, Table} from "react-bootstrap";
 
 export default class BookList extends Component{
     render() {
         return(
-            <div className="text-white-50">Add Book List</div>
+                <Card className={"border border-dark bg-white text-white"}>
+                    <Card.Header className={"text-dark"}>
+                        Book List
+                    </Card.Header>
+                    <Card.Body>
+                        <Table bordered hover striped variant="dark">
+                            <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>ISBN Number</th>
+                                <th>Price</th>
+                                <th>Language</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr align={"center"}>
+                                <td colSpan={6}>No Books Available</td>
+
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
         );
     }
 }
