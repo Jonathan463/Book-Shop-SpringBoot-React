5013cf7795cc47c968e9eb459b4ddab3954c0972
decagon <godsonjonathan39@gmail.com>
2022-02-16 02:45:45 +0100

    Made major Changes, added form,table,axios,componentDidMount

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,96 @@
 import React, {Component} from 'react';
 import {Button, Card, Col, Form, Row} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlusSquare, faSave} from '@fortawesome/free-solid-svg-icons';
 
 export default class Book extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {title:'', author:'', coverPhotoURL:'', isbnNumber:'', price:'', language:''};
+        this.bookChange = this.bookChange.bind(this);
+        this.submitBook = this.submitBook.bind(this);
+    }
+    submitBook(event){
+        alert('Title: '+this.state.title+', Author: '+this.state.author+', Cover Photo URL: '+this.state.coverPhotoURL+", ISBN Number: "+this.state.isbnNumber+', Price: '+this.state.price+", Language: "+this.state.language);
+        event.preventDefault();
+    }
+    //this method is used to prevent the values from displaying on the url
+    bookChange(event){
+        this.setState({
+            [event.target.name]:event.target.value
+        });
+    }
     render(){
         return(
             <Card className={"border border-dark bg-white text-white"}>
-                <Card.Header className={"text-dark"}>Add Book</Card.Header>
-                <Form>
+                <Card.Header className={"text-dark"}><FontAwesomeIcon icon={faPlusSquare} />Add Book</Card.Header>
+                <Form onSubmit={this.submitBook} id="bookFormId">
                     <Card.Body>
                        <Row>
-                           <Form.Group as={Col}>
-                               <Form.Label>Title</Form.Label>
-                               <Form.Control type="text" placeholder="Enter Title" />
+                           <Form.Group as={Col} controlId="formGridTitle">
+                               <Form.Label className={"text-dark"}>Title</Form.Label>
+                               <Form.Control required
+                                   type="text"
+                                   placeholder="Enter Title"
+                                   name="title"
+                                             value={this.state.title}
+                                   onChange={this.bookChange}/>
                            </Form.Group>
-                           <Form.Group as={Col}>
-                               <Form.Label>Author</Form.Label>
-                               <Form.Control type="text" placeholder="Enter Author" />
+                           <Form.Group as={Col} controlId="formGridAuthor">
+                               <Form.Label className={"text-dark"}>Author</Form.Label>
+                               <Form.Control required
+                                   type="text"
+                                   placeholder="Enter Author"
+                                   name="author"
+                                             value={this.state.author}
+                                             onChange={this.bookChange}
+                               />
                            </Form.Group>
                        </Row>
+                        <Row>
+                            <Form.Group as={Col} controlId="formGridCoverPhotoURL">
+                                <Form.Label className={"text-dark"}>Cover Photo URL</Form.Label>
+                                <Form.Control required
+                                    type="text"
+                                    placeholder="Enter Book Cover Photo URL"
+                                    name="coverPhotoURL"
+                                              value={this.state.coverPhotoURL}
+                                              onChange={this.bookChange}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridISBNNumber">
+                                <Form.Label className={"text-dark"}>ISBN Number</Form.Label>
+                                <Form.Control required
+                                    type="text"
+                                    placeholder="Enter Book ISBN Number"
+                                    name="isbnNumber"
+                                              value={this.state.isbnNumber}
+                                              onChange={this.bookChange}/>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} controlId="formGridPrice">
+                                <Form.Label className={"text-dark"}>Price</Form.Label>
+                                <Form.Control required
+                                    type="text"
+                                    placeholder="Enter Book Price"
+                                    name="price"
+                                              value={this.state.price}
+                                              onChange={this.bookChange}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridLanguage">
+                                <Form.Label className={"text-dark"}>Language</Form.Label>
+                                <Form.Control required
+                                    type="text"
+                                    placeholder="Enter Book Language"
+                                    name="language"
+                                              value={this.state.language}
+                                              onChange={this.bookChange}/>
+                            </Form.Group>
+                        </Row>
                     </Card.Body>
                     <Card.Footer style={{"textAlign":"right"}}>>
                         <Button variant="primary" type="submit">
-                            Submit
+                            <FontAwesomeIcon icon={faSave} />Submit
                         </Button>
                     </Card.Footer>
                 </Form>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
-import {Card, Table} from "react-bootstrap";
+import {Button, ButtonGroup, Card, Table} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faList, faTrash} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 export default class BookList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            books : []
+        };
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/rest/books")
+            .then(response => response.data)
+        .then((data) =>{
+            this.setState({books: data})
+        })
+    }
+
     render() {
         return(
                 <Card className={"border border-dark bg-white text-white"}>
-                    <Card.Header className={"text-dark"}>
-                        Book List
-                    </Card.Header>
+                    <Card.Header className={"text-dark"}><FontAwesomeIcon icon={faList} />Book List</Card.Header>
                     <Card.Body>
                         <Table bordered hover striped variant="dark">
                             <thead>
@@ -21,10 +37,30 @@ export default class BookList extends Component{
                             </tr>
                             </thead>
                             <tbody>
-                            <tr align={"center"}>
+                            {
+                                this.state.books.length === 0 ?
+                            <tr align="center">
                                 <td colSpan={6}>No Books Available</td>
+                            </tr> : this.state.books.map((book) =>(
+                                <tr key={book.id}>
 
-                            </tr>
+                                <td>
+                                    <image src={book.coverPhotoURL} roundedCircle width="25" height="25"/>
+                                    {book.title}
+                                </td>
+                                <td>{book.author}</td>
+                                <td>{book.isbnNumber}</td>
+                                <td>{book.price}</td>
+                                <td>{book.language}</td>
+                                <td>
+                                    <ButtonGroup>
+                                        <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}/></Button>
+                                        <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}/></Button>
+                                    </ButtonGroup>
+                                </td>
+                                </tr>
+                                ))
+                            }
                             </tbody>
                         </Table>
                     </Card.Body>
