6d170f95f930c9e2d80e4adee85fd25fe3caf369
decagon <godsonjonathan39@gmail.com>
2022-02-17 22:51:39 +0100

    modified Book state

--- src/components/Book.js
+++ src/components/Book.js
@@ -21,6 +21,7 @@ export default class Book extends Component{
         });
     }
     render(){
+        const {title, author, coverPhotoURL, isbnNumber,price, language} = this.state;
         return(
             <Card className={"border border-dark bg-white text-white"}>
                 <Card.Header className={"text-dark"}><FontAwesomeIcon icon={faPlusSquare} />Add Book</Card.Header>
@@ -29,62 +30,49 @@ export default class Book extends Component{
                        <Row>
                            <Form.Group as={Col} controlId="formGridTitle">
                                <Form.Label className={"text-dark"}>Title</Form.Label>
-                               <Form.Control required
+                               <Form.Control required autoComplete="off"
                                    type="text"
-                                   placeholder="Enter Title"
-                                   name="title"
-                                             value={this.state.title}
-                                   onChange={this.bookChange}/>
+                                   placeholder="Enter Title" name="title" value={title} onChange={this.bookChange}/>
                            </Form.Group>
                            <Form.Group as={Col} controlId="formGridAuthor">
                                <Form.Label className={"text-dark"}>Author</Form.Label>
-                               <Form.Control required
+                               <Form.Control required autoComplete="off"
                                    type="text"
                                    placeholder="Enter Author"
-                                   name="author"
-                                             value={this.state.author}
-                                             onChange={this.bookChange}
+                                   name="author" value={this.state.author} onChange={this.bookChange}
                                />
                            </Form.Group>
                        </Row>
                         <Row>
                             <Form.Group as={Col} controlId="formGridCoverPhotoURL">
                                 <Form.Label className={"text-dark"}>Cover Photo URL</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text"
                                     placeholder="Enter Book Cover Photo URL"
-                                    name="coverPhotoURL"
-                                              value={this.state.coverPhotoURL}
-                                              onChange={this.bookChange}/>
+                                    name="coverPhotoURL" value={coverPhotoURL} onChange={this.bookChange}/>
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridISBNNumber">
                                 <Form.Label className={"text-dark"}>ISBN Number</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text"
                                     placeholder="Enter Book ISBN Number"
-                                    name="isbnNumber"
-                                              value={this.state.isbnNumber}
-                                              onChange={this.bookChange}/>
+                                    name="isbnNumber" value={isbnNumber} onChange={this.bookChange}/>
                             </Form.Group>
                         </Row>
                         <Row>
                             <Form.Group as={Col} controlId="formGridPrice">
                                 <Form.Label className={"text-dark"}>Price</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text"
                                     placeholder="Enter Book Price"
-                                    name="price"
-                                              value={this.state.price}
-                                              onChange={this.bookChange}/>
+                                    name="price" value={price} onChange={this.bookChange}/>
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridLanguage">
                                 <Form.Label className={"text-dark"}>Language</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text"
                                     placeholder="Enter Book Language"
-                                    name="language"
-                                              value={this.state.language}
-                                              onChange={this.bookChange}/>
+                                    name="language" value={language} onChange={this.bookChange}/>
                             </Form.Group>
                         </Row>
                     </Card.Body>
